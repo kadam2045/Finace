5cb761230a1a5857c77e32bb66c53749640f50e8
Shubham Kadam <shubham.kadam@appgambit.com>
2024-07-03 14:55:45 +0530

    first commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>signUp Page</div>;
+};
+
+export default page;
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return <p className="py-3">this is authenticate route </p>;
+};
+
+export default page;
